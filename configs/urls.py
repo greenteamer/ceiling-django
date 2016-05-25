351d489a9fa5fde42d0af1d7528823a9b879ee86
greenteamer <greenteamer@bk.ru>
2016-05-25 17:25:51 +0300

    yandex confirm

--- configs/urls.py
+++ configs/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 	url(r"^contact-form/$", contact_form_view, name="contact_form_view"),
 	url(r"^ceiling-form/$", ceiling_form_view, name="ceiling_form_view"),
 	url(r"^ajax/zamer/$", ajax_zamer_view, name="ajax_zamer_view"),
-	url(r"^ajax/calculator/$", ajax_calculator_view, name="ajax_calculator_view")
+	url(r"^ajax/calculator/$", ajax_calculator_view, name="ajax_calculator_view"),
+	url(r'^ab730baf57b8\.html$', yandex_confirm_view, name="yandex_confirm_view")
 ]
